ce1d417e6bc630063fc22081d418d25ad1d93b4b
Simon K Zheng <simonkzheng@gmail.com>
2018-01-31 20:04:39 -0500

    Widget formatted

--- src/Components/Widget/Widget.js
+++ src/Components/Widget/Widget.js
@@ -10,20 +10,43 @@ class Widget extends React.Component {
    this.state = {
      dummy: [
        {}
-     ]
-   };
+     ],
+     eventTitle: '',
+     eventCategory: '',
+     eventLocation: '',
+     eventImageURL: ''
+   }
+   this.handleGetEvents = this.handleGetEvents.bind(this);
+   this.handleAddEvent = this.handleAddEvent.bind(this);
+ }
+
+ componentDidMount(){
+   this.handleGetEvents();
  }
 
  handleAddEvent(){
+   axios.get('/events')
+   .then((response) => {
+     console.log(response.data);
+   })  
+   .catch((error) => {
+     console.log(error);
+   })
+ }
+
+ handleGetEvents(){
 
  }
 
  render() {
    return (
      <div id="widget">
-       <div id="widget-header"></div>
-      <p>hey</p>
-      
+        <div id="widget-header">
+         <a href="https://pulsd.com/new-york"><img id="logo" src="/assets/logo-125.png"/></a>
+         <img id="admin" src="/assets/admin.png" />
+        </div>
+        <p id="widget-title" className="small">EXPLORE EVENTS NEARBY</p>
+        <p id="footer" className="text-muted small">Powered by Pulsd</p>
      </div>
 
    );
